a2d63225c5ce5bab9b3651a7d2bf17ec25e7bf11
yashanand167 <yash.anand167@gmail.com>
2026-08-11 02:12:14 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-background font-sans text-foreground">
+        {children}
+      </body>
     </html>
   );
 }
