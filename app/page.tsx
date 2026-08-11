608227c66f4793099a92878f29dcb3bde5ebad9f
yashanand167 <yash.anand167@gmail.com>
2026-08-11 23:29:23 +0530

    Update global styles and modify homepage layout. Changed font variables in CSS and adjusted background color. Updated the homepage to display buttons instead of a simple greeting.

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main className="flex items-center justify-center h-screen gap-2">
+    <button className="aspect-square bg-white size-12 rounded-md">
+      <h1 className="text-xl text-neutral-500">Q</h1>
+    </button>
+    <button className="aspect-square bg-white size-12 rounded-md">
+      <h1 className="text-xl text-neutral-500">W</h1>
+    </button>
+    </main>
   );
 }
---
+++ hooks/use-mobile.ts
@@ -0,0 +1,24 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+const MOBILE_BREAKPOINT = 768
+
+export function useMobile() {
+  const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    }
+
+    mediaQuery.addEventListener("change", onChange)
+    onChange()
+
+    return () => mediaQuery.removeEventListener("change", onChange)
+  }, [])
+
+  return !!isMobile
+}
